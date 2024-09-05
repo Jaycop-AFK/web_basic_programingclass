d3481c840829315b44454329707f9aec88262fe1
Jaycop-AFK <82388750+Jaycop-AFK@users.noreply.github.com>
2024-09-05 16:37:59 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const allinks = document.querySelectorAll("a:link");
+
+allinks.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#")
+      window.scrollTo({
+        behavior: "smooth",
+        top: 0,
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+      console.log(sectionEl);
+    }
+  });
+});
+
+
+const sectionHero = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function(entries) {
+    const ent = entries[0];
+    if(ent.isIntersecting == false){
+      document.body.classList.add("sticky");
+    }
+    if(ent.isIntersecting == true){
+      document.body.classList.remove("sticky");
+    }
+  },{
+    root: null,
+    threshold: 0,
+    rootMargin:"-80px",
+  }
+)
+
+obs.observe(sectionHero);
